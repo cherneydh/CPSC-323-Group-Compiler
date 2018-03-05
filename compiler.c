2eda4e581a0b02cb476759c6548ac3d212782cce
Daniel Cherney <31091775+cherneydh@users.noreply.github.com>
2018-03-05 11:23:49 -0800

    Comments and minor changes

--- compiler.c
+++ compiler.c
@@ -6,33 +6,40 @@
 /* f &= to off */
 
 int main(){
+	/* Declare variables */
 	int c;
 	int prev_c;
 	FILE * source;
 	FILE * dest;
-	
+
+	/* Open source and destination files */
 	source = fopen("final.txt", "r");
 	dest = fopen("final1.txt", "w");
 	
-	if(source != NULL){
+	if((source != NULL) && (dest != NULL)){
 		while((c = fgetchar()) != EOF){
+			/* Set the flag that we are in a comment if these conditions are true */
 			if(c == '*' && prev_c == '('){
 				flags |= COMMENT;
 			}
 			
+			/* Set the flag that we left the comment if these conditions are true */
 			if(c == ')' && prev_c == '*'){
 				flags &= COMMENT;
 			}
 			
+			/* If there is white space after a newline, ignore. */
 			if(isspace(c) && prev_c == '\n'){
 				continue;
 			}
 			
+			/* If we are in a comment, ignore the character. */
 			if(flags & COMMENT){
 				prev_c = c;
 				continue;
 			}
 			
+			/* Print char to destination file */
 			fprintf(dest, c);
 			
 			prev_c = c;
