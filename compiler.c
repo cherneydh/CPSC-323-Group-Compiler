def02eeecd5cdb6de8d118e168f77288dacc7573
Daniel Cherney <31091775+cherneydh@users.noreply.github.com>
2018-03-05 09:55:45 -0800

    Two files. One read, one write.

--- compiler.c
+++ compiler.c
@@ -8,8 +8,10 @@ int main(){
 	int c;
 	int prev_c;
 	FILE * source;
+	FILE * dest;
 	
 	source = fopen("final.txt", "r");
+	dest = fopen("final1.txt", "w");
 	
 	if(source != NULL){
 		while((c = fgetchar()) != EOF){
@@ -21,12 +23,21 @@ int main(){
 				flags &= COMMENT;
 			}
 			
+			if(c == '\n' && prev_c == '\n'){
+				continue;
+			}
+			
 			if(flags & COMMENT){
 				prev_c = c;
 				continue;
 			}
 			
+			fprintf(dest, c);
+			
 			prev_c = c;
 		}
 	}
+	
+	fclose(source);
+	fclose(dest);
 }
