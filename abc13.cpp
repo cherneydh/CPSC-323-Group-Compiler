3ddc354d82dbda6cf77a9b02eee6f34d8754fc03
Daniel Cherney <31091775+cherneydh@users.noreply.github.com>
2018-03-05 09:39:00 -0800

    Added files from project specification

---
+++ abc13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+Using namespace std;
+int main()
+{
+int ab5, cb, be, eb;
+ab5 = 5
+cout << ”ab5=” << ab5;
+eb = cb + ab5;
+cout << eb;
+be = 2 * ab5 + eb;
+cout << be;
+return 0;
+}
